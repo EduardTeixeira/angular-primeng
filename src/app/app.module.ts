c08ba4b7a965317620fdcab1ab74b7fbcce7f04e
Eduard Teixeira <eduard.teixeira01@gmail.com>
2022-04-14 17:18:53 -0300

    configs do primeng e bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from 'primeng/button';
 import { CarouselModule } from 'primeng/carousel';
 import { MenubarModule } from 'primeng/menubar';
+import { FieldsetModule } from 'primeng/fieldset';
+import { CardModule } from 'primeng/card';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +28,12 @@ import { ButtonsComponent } from './buttons/buttons.component';
     ButtonModule,
     CarouselModule,
     MenubarModule,
+    FieldsetModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
   ],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
     NO_ERRORS_SCHEMA
